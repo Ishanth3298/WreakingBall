81e15416a47cf109af8f30692ec612c5dc1bc29c
Ishanth3298 <75448742+Ishanth3298@users.noreply.github.com>
2021-02-18 15:10:18 -0500

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,24 @@
+class Ball {
+    constructor(x,y,width,height) {
+        var options = {
+            frictionAir: 0.005,
+            density: 1
+        }
+        this.body = Bodies.rectangle(x,y, width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+    display() {
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        strokeWeight(7);
+        stroke("purple");
+        fill("red")
+        ellipseMode(RADIUS)
+        ellipse(0,0,this.width,this.height);
+        pop();
+    }
+}
